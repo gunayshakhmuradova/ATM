028d805cb1f55e084e929fc6a2031a5cc9cdfb19
Gunay Zahra <126316477+shzehra93@users.noreply.github.com>
2023-08-10 11:49:24 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -12,30 +12,33 @@ var hesabB = {
     əlavəHesab : 2000
 }
 
-function pulÇıxartma(hesab,miqdar){
+function pulÇıxartma(hesab, miqdar) {
     console.log(`Salam ${hesab.ad}`);
-    if(hesab.balans>=miqdar){
+    if (hesab.balans >= miqdar) {
         hesab.balans = hesab.balans - miqdar;
         console.log('Pulunuzu çıxardın.');
-    }
-    else {
+    } else {
         var toplam = hesab.balans + hesab.əlavəHesab;
-       if(toplam >=miqdar){
-        if(confirm(`Əlavə balansınızdan istifadə etmək istəyirsiniz mi?`)){console.log('Pulunuzu çıxarda bilərsiniz.');
-        var balans = hesab.balans;
-        var əlavəHesab = miqdar - balans;
-        hasab.balans = 0;
-        hesab.əlavəHesab = hesab.əlavəHesab - əlavəHesab;
-    } else{
-            console.log(`${hesab.hesabNo} nömrəli hesabınızda ${miqdar} yoxdur.`);
+        if (toplam >= miqdar) {
+            if (confirm(`Əlavə balansınızdan istifadə etmək istəyirsiniz mi?`)) {
+                console.log('Pulunuzu çıxarda bilərsiniz.');
+                var balans = hesab.balans;
+                var əlavəHesab = miqdar - balans;
+                hesab.balans = 0;
+                hesab.əlavəHesab = hesab.əlavəHesab - əlavəHesab;
+            } else {
+                console.log(`${hesab.hesabNo} nömrəli hesabınızda ${miqdar} yoxdur.`);
+            }
+        } else {
+            console.log('Balansınızda kifayət qədər məbləğ yoxdur.');
         }
-        }else{
-        console.log('Balansınızda kifayət qədər məbləğ yoxdur.');
     }
-    }
-    
 }
 
-pulÇıxartma (hesabA, 2000);
-pulÇıxartma (hesabA, 2000);
-// pulÇıxartma (hesabB, 2000);
+document.getElementById("btnHesabA").addEventListener("click", function() {
+    pulÇıxartma(hesabA, 2000);
+});
+
+document.getElementById("btnHesabB").addEventListener("click", function() {
+    pulÇıxartma(hesabB, 2000);
+});
